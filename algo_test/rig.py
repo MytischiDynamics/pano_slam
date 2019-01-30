7cf370fc1d974166a95ac6e649aee287ed74ee7f
Юрий Морозов <morozov@inviewlab.com>
2019-01-31 00:17:55 +0300

    Basic structs

---
+++ algo_test/rig.py
@@ -0,0 +1,12 @@
+import camera
+
+class Rig:
+    def __init__(self):
+        self.cameras = {}
+        self.stereo_cameras = {}
+        self.max_camera_idx = 0
+        self.max_stereo_camera_idx = 0
+
+    def AddCamera(self, camera_struct):
+        self.camereas[self.max_camera_idx] = camera_struct
+        self.max_camera_idx += 1

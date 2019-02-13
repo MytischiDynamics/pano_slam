ba5555e0f58b819fb052695737b110b54f581129
Yury Morozov <morozov.uv@gmail.com>
2019-02-13 08:23:22 +0300

    Small addition to optimization window structures

--- algo_test/frame_window.py
+++ algo_test/frame_window.py
@@ -1,15 +1,16 @@
 import cv2 as cv
+from camera import Camera
 from rig import Rig
 
 class FrameWindow:
     def __init__(self):
-        self.frames_list = []
+        self.frames_queue = []
 
     def push_frames_pack(self, frames_pack):
-        self.frames_list.append(frames_pack)
+        self.frames_queue.append(frames_pack)
 
     def remove_frames_pack(self, idx):
-        self.frames_list = self.frames_list[:idx] + self.frames_list[idx:]
+        self.frames_queue = self.frames_queue[:idx] + self.frames_queue[idx:]
 
 
 #Pack of frames simultaneously obtained from camera rig
@@ -17,4 +18,13 @@ class FramesPack:
     def __init__(self, indicies):
         self.frames = {}
         for idx in indicies:
-            self.frames[idx] = cv.Mat()
+            self.frames[idx] = Frame()
+
+
+class Frame:
+    def __init__(self, pic):
+        self.image = pic
+        self.camera = Camera()
+
+    def __init__(self):
+        self.image = cv.Mat()
